cc20db4e19a2dd2a990dcbf00945ef7b973eda37
Montserrat Molina <mgmolina3@miners.utep.edu>
2022-02-18 15:13:19 -0700

    added test cases

--- triangle/test/triangle_test.dart
+++ triangle/test/triangle_test.dart
@@ -2,7 +2,85 @@ import 'package:triangle/triangle.dart';
 import 'package:test/test.dart';
 
 void main() {
+  test('Non-Integer inputs', () {
+    expect(typeOfTriangle('a', '-', '10'),
+        equals('Error: One or more of the provided inputs are not integers.'));
+  });
+
+  test('Non-Integer inputs', () {
+    expect(typeOfTriangle('9', '10', ';'),
+        equals('Error: One or more of the provided inputs are not integers.'));
+  });
+
+  test('Out of Range inputs', () {
+    expect(
+        typeOfTriangle('9', '-1', '100'),
+        equals(
+            'Error: One or more of the provided inputs are not within the inclusive range [1-200].'));
+  });
+
+  test('Out of Range inputs', () {
+    expect(
+        typeOfTriangle('900', '900', '900'),
+        equals(
+            'Error: One or more of the provided inputs are not within the inclusive range [1-200].'));
+  });
+
+  test('Out of Range inputs', () {
+    expect(
+        typeOfTriangle('0', '1', '2'),
+        equals(
+            'Error: One or more of the provided inputs are not within the inclusive range [1-200].'));
+  });
+
+  test('Out of Range inputs', () {
+    expect(
+        typeOfTriangle('200', '200', '201'),
+        equals(
+            'Error: One or more of the provided inputs are not within the inclusive range [1-200].'));
+  });
+
+  test('NotATriangle', () {
+    expect(typeOfTriangle('1', '2', '10'), equals('NotATriangle'));
+  });
+
+  test('NotATriangle', () {
+    expect(typeOfTriangle('1', '2', '3'), equals('NotATriangle'));
+  });
+
+  test('NotATriangle', () {
+    expect(typeOfTriangle('100', '200', '3'), equals('NotATriangle'));
+  });
+
   test('Equilateral', () {
-    expect(typeOfTriangle(1, 1, 1), equals('Equilateral'));
+    expect(typeOfTriangle('1', '1', '1'), equals('Equilateral'));
+  });
+
+  test('Equilateral', () {
+    expect(typeOfTriangle('100', '100', '100'), equals('Equilateral'));
+  });
+
+  test('Isosceles', () {
+    expect(typeOfTriangle('100', '100', '99'), equals('Isosceles'));
+  });
+
+  test('Isosceles', () {
+    expect(typeOfTriangle('4', '5', '4'), equals('Isosceles'));
+  });
+
+  test('Isosceles', () {
+    expect(typeOfTriangle('10', '10', '12'), equals('Isosceles'));
+  });
+
+  test('Scalene', () {
+    expect(typeOfTriangle('13', '10', '12'), equals('Scalene'));
+  });
+
+  test('Scalene', () {
+    expect(typeOfTriangle('3', '4', '5'), equals('Scalene'));
+  });
+
+  test('Scalene', () {
+    expect(typeOfTriangle('99', '100', '115'), equals('Scalene'));
   });
 }
